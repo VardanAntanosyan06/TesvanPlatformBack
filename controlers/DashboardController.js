e5b4119bcc3ceba1a8f24455461759bed3654602
VardanAntanosyan06 <vantanosyan99@gmail.com>
2024-03-28 11:16:49 +0400

    changed dashboard logic

--- controlers/DashboardController.js
+++ controlers/DashboardController.js
@@ -1,23 +1,72 @@
-const { Users, Groups } = require('../models');
+const { Users, Groups, GroupsPerUsers } = require("../models");
 
 const getUserStatictis = async (req, res) => {
   try {
     const { id } = req.params;
-    const statics = await Users.findAll({
+    const { user_id: userId } = req.user;
+
+    const { lessons, homeWork, quizzes } = await GroupsPerUsers.findOne({
       where: {
-        id,
+        userId,
+        groupId: id,
       },
-      include: [
+    });
+
+    const response = {
+      lessons,
+      homeWork,
+      quizzes,
+      totalPoints: (lessons + homeWork + quizzes) / 3,
+      mySkils: [
         {
-          model: Groups,
-          as: 'groups',
+          name: "Communication",
+          point: 10,
+        },
+        {
+          name: "Test case execution",
+          pont: 80,
+        },
+        {
+          name: "Agile methodology",
+          point: 100,
+        },
+        {
+          name: "Scrum methodology",
+          point: 100,
+        },
+        {
+          name: "Cypress",
+          point: 10,
         },
       ],
-    });
-    return res.json(statics);
+      course: {
+        students: "21 stuundet",
+        lessons: "30 lessons",
+        course: "1 course",
+      },
+      daily: {
+        quizz: [
+          {
+            id: 1,
+            title: "Lorem ipsum",
+            description: "Lorem ipsum",
+          },
+        ],
+        homework: [
+          {
+            id: 1,
+            title: "test",
+            description: "test",
+          },
+        ],
+      },
+    };
+    return res.json(response);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ success: false, message: 'Something Went Wrong' });
+    return res
+      .status(500)
+      .json({ success: false, message: "Something Went Wrong" });
   }
 };
 
