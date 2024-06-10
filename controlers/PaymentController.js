4170319c1a1c9e1877e1de854b94632340b7d024
VardanAntanosyan06 <vantanosyan99@gmail.com>
2024-06-10 19:23:53 +0400

    Idram logic

--- controlers/PaymentController.js
+++ controlers/PaymentController.js
@@ -303,12 +303,178 @@ const ConfirmIdram = async (request, res) => {
         let currentDate = new Date();
         currentDate.setFullYear(currentDate.getFullYear() + 1);
         currentDate = currentDate.toISOString();
-        // let UserSubscribtionPayment = await SubscribtionPayment.findOne({
-        //   where: { id: request.EDP_BILL_NO },
-        // });
-        // UserSubscribtionPayment.endDate = currentDate;
-        // UserSubscribtionPayment.save();
-        // }
+        let payment = await Payment.findOne({
+          where: { orderKey: request.EDP_BILL_NO },
+        });
+        if (!payment)
+          return res.status(400).json({ success: false, message: 'Payment does not exist' });
+        payment.status = paymentResponse.errorMessage;
+    
+        payment.save();
+  
+        if (payment.type == 'Group') {
+          const user = await Users.findOne({ where: { id: payment.userId } });
+          const group = await Groups.findByPk(payment.groupId);
+          if (!group) {
+            return res.json({ success: false, message: 'Group not found' });
+          }
+    
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+          const {role} = await Users.findByPk(payment.userId)
+          await GroupsPerUsers.findOrCreate({
+            where:{  
+              groupId: payment.groupId,
+              userId: payment.userId,
+            },
+            defaults:{
+              groupId: payment.groupId,
+              userId: payment.userId,
+              userRole: role,
+            }
+          });
+    
+          await UserCourses.create({
+            GroupCourseId: group.assignCourseId,
+    
+            UserId: payment.userId,
+          });
+          const lessons = await CoursesPerLessons.findAll({
+            where: { courseId: group.assignCourseId },
+          });
+          await UserPoints.findOrCreate({
+            where: {
+              userId: payment.userId,
+            },
+            defaults: {
+              userId: payment.userId,
+              lesson: 0,
+              quizz: 0,
+              finalInterview: 0,
+            },
+          });
+          lessons.map((e) => {
+            UserLesson.create({
+              GroupCourseId: group.assignCourseId,
+              UserId: payment.userId,
+              LessonId: e.lessonId,
+            });
+          });
+    
+          const Course = await GroupCourses.findOne({
+            where:{id:group.assignCourseId},
+            include: [
+              {
+                model: Lesson,
+                include: [
+                  {
+                    model: Homework,
+                    as: "homework",
+                  },
+                ],
+                required: true,
+              },
+            ],
+          })  
+    
+          Course.Lessons.map((lesson)=>{
+            UserHomework.create({
+              GroupCourseId: group.assignCourseId,
+              UserId:payment.userId,
+              HomeworkId: lesson.homework[0].id,
+              points: 0,
+            })
+          })
+          const boughtTests = await Tests.findAll({
+            where: {
+              [sequelize.Op.or]: [{ courseId: group.assignCourseId }, { courseId: null }],
+            },
+          });
+    
+          boughtTests.map((test) => {
+            UserTests.findOrCreate({
+              where: {
+                testId: test.id,
+                userId: payment.userId,
+                courseId: test.courseId,
+                language: test.language,
+                type: 'Group',
+              },
+              defaults: {
+                testId: test.id,
+                userId: payment.userId,
+              },
+            });
+          });
+    
+          const groupChats = await GroupChats.findOne({
+            where: { groupId: payment.groupId },
+          });
+          const newMembers = [payment.userId, ...groupChats.members];
+          const uniqueUsers = [...new Set(newMembers)];
+          groupChats.members = uniqueUsers;
+    
+          await groupChats.save();
+    
+          res.send({ success: true });
+        } else if (payment.type == 'Individual') {
+          const user = await Users.findOne({ where: { id: payment.userId } });
+          const course = await GroupCourses.findByPk(payment.groupId);
+          if (!course) {
+            return res.status(404).json({ success: false, message: 'Course not found' });
+          }
+    
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+          await UserCourses.create({
+            GroupCourseId: payment.groupId,
+            UserId: payment.userId,
+          });
+          const lessons = await CoursesPerLessons.findAll({
+            where: { courseId: payment.groupId },
+          });
+          await UserPoints.findOrCreate({
+            where: {
+              userId: payment.userId,
+            },
+            defaults: {
+              userId: payment.userId,
+              lesson: 0,
+              quizz: 0,
+              finalInterview: 0,
+            },
+          });
+          lessons.map((e) => {
+            UserLesson.create({
+              GroupCourseId: payment.groupId,
+              UserId: payment.userId,
+              LessonId: e.lessonId,
+            });
+          });
+    
+          const boughtTests = await Tests.findAll({
+            where: {
+              [sequelize.Op.or]: [{ courseId: payment.groupId }, { courseId: null }],
+            },
+          });
+    
+          boughtTests.map((test) => {
+            UserTests.findOrCreate({
+              where: {
+                testId: test.id,
+                userId: payment.userId,
+                courseId: test.courseId,
+                language: test.language,
+                type: 'Group',
+              },
+              defaults: {
+                testId: test.id,
+                userId: payment.userId,
+              },
+            });
+          });
       }
     }
   }
