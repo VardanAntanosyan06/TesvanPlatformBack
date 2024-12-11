121c38c0f310113de1cead182f3b2325eb91407e
mhertovm <mhertovmasyan904@gmail.com>
2024-12-11 16:04:44 +0400

    userHomwork migration

--- migrations/20241204084301-change-userHomework-points-type-to-numeric.js
+++ migrations/20241204084301-change-userHomework-points-type-to-numeric.js
@@ -5,6 +5,8 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.changeColumn('UserHomeworks', 'points', {
       type: Sequelize.NUMERIC,
+      defaultValue: 0,
+      allowNull: false,
     });
   },
 
