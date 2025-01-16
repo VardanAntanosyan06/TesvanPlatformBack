ebc8da11c1f04056c5b015df5b6dbbff412df17b
mhertovm <mhertovmasyan904@gmail.com>
2025-01-16 10:56:59 +0400

    added upload mg

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(express.static(path.resolve(__dirname, 'messageFiles')));
