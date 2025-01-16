c28911466bf8eca9001c08c37bb89db154774628
mhertovm <mhertovmasyan904@gmail.com>
2025-01-16 11:00:03 +0400

    added upload mg

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(logger('dev'));
+app.use(fileUpload({
+  limits: { fileSize: 400 * 1024 * 1024 }  //400MB
+}));
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: false }));
 app.use(cookieParser());
@@ -60,10 +63,6 @@ app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(express.static(path.resolve(__dirname, 'messageFiles')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'messageFiles')));
-
-app.use(fileUpload({
-  limits: { fileSize: 400 * 1024 * 1024 }  //400MB
-}));
 
 app.use('/', indexRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
