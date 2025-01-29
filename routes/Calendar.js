33c1b94ecebc4a318b12013f920a6630b758d219
mhertovm <mhertovmasyan904@gmail.com>
2025-01-29 15:04:49 +0400

    calendar superadmin

--- routes/Calendar.js
+++ routes/Calendar.js
@@ -4,18 +4,18 @@ var router = express.Router();
 const controller = require("../controlers/CalendarController");
 const checkAuth = require("../middleware/checkAuth");
 
-router.post("/create", checkAuth(["TEACHER", "ADMIN"]), controller.create);
-router.get("/findOne/:id", checkAuth(["STUDENT", "TEACHER", "ADMIN"]), controller.findOne);
-router.get("/findByDay", checkAuth(["STUDENT", "TEACHER", "ADMIN"]), controller.findByDay);
-router.get("/findByWeek", checkAuth(["STUDENT", "TEACHER", "ADMIN"]), controller.findByWeek);
-router.get("/getUsers", checkAuth(["TEACHER", "ADMIN"]), controller.getUsers);
-router.get("/getUsersForTeacher", checkAuth(["TEACHER"]), controller.getUsersForTeacher);
+router.post("/create", checkAuth(["TEACHER", "ADMIN", "SUPERADMIN"]), controller.create);
+router.get("/findOne/:id", checkAuth(["STUDENT", "TEACHER", "ADMIN", "SUPERADMIN"]), controller.findOne);
+router.get("/findByDay", checkAuth(["STUDENT", "TEACHER", "ADMIN", "SUPERADMIN"]), controller.findByDay);
+router.get("/findByWeek", checkAuth(["STUDENT", "TEACHER", "ADMIN", "SUPERADMIN"]), controller.findByWeek);
+router.get("/getUsers", checkAuth(["TEACHER", "ADMIN", "SUPERADMIN"]), controller.getUsers);
+router.get("/getUsersForTeacher", checkAuth(["TEACHER", "SUPERADMIN"]), controller.getUsersForTeacher);
 
-router.get("/findByMonth", checkAuth(["STUDENT", "TEACHER", "ADMIN"]), controller.findByMonth);
-router.get("/findByYear", checkAuth(["STUDENT", "TEACHER", "ADMIN"]), controller.findByYear);
-router.get("/findAll", checkAuth(["TEACHER", "ADMIN"]), controller.findAll);
-router.patch("/update", checkAuth(["TEACHER", "ADMIN"]), controller.update);
-router.delete("/remove/:id", checkAuth(["TEACHER", "ADMIN"]), controller.remove);
+router.get("/findByMonth", checkAuth(["STUDENT", "TEACHER", "ADMIN", "SUPERADMIN"]), controller.findByMonth);
+router.get("/findByYear", checkAuth(["STUDENT", "TEACHER", "ADMIN", "SUPERADMIN"]), controller.findByYear);
+router.get("/findAll", checkAuth(["TEACHER", "ADMIN", "SUPERADMIN"]), controller.findAll);
+router.patch("/update", checkAuth(["TEACHER", "ADMIN", "SUPERADMIN"]), controller.update);
+router.delete("/remove/:id", checkAuth(["TEACHER", "ADMIN", "SUPERADMIN"]), controller.remove);
 
 
 
